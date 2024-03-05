7e462cdad5f27f8085eeb899baf4c46219726bcb
WenjiaoYue <wenjiao.yue@intel.com>
2024-03-05 14:51:08 +0800

    update streamlit code

---
+++ Streamlit/pages/3_📙_Chat_With_Your_PDF.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# embed streamlit docs in a streamlit app
+components.iframe("http://10.255.28.43:5174/", height=500, scrolling=True)
